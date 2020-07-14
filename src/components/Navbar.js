bbfa40ddc7d251904d9f8e3dcffef38fd99a7014
Alexander M <iammorjj@gmail.com>
2020-07-14 04:22:56 +0300

    Programming redirect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import {Link, NavLink} from 'react-router-dom'
+import {Link, NavLink, withRouter} from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = (props) => {
+  setTimeout(() => {
+    props.history.push('/about')
+  }, 2000)
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
@@ -12,8 +15,8 @@ const Navbar = () => {
           <li><NavLink to="/contact">Contact</NavLink></li>
         </ul>
       </div>
-    </nav> 
+    </nav>
   )
 }
 
-export default Navbar
+export default withRouter(Navbar)
